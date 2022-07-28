769abe3721fad89492b6630375e159c2f5ee1355
TrevorSlobodnick <trevorslobodnick@gmail.com>
2022-07-28 05:05:55 -0400

    Still widdling down the fonts to use, added some demo paragraphs to see what they look like

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,9 +5,11 @@ import "react-toggle/style.css"
 const Footer = ({ darkMode, setDarkMode }) => {
   return (
     <footer>
+        <p>Quis ipsum voluptate consequat amet tempor pariatur. Officia laboris amet tempor sint laborum adipisicing. Laboris laboris culpa aliqua anim voluptate velit dolor dolore quis in. Sint incididunt cillum ut Lorem ad esse ad occaecat commodo exercitation aliquip. Non enim labore elit mollit Lorem laborum labore ut cupidatat nulla eiusmod magna aliquip eiusmod. Ullamco aliquip labore in eu non velit sunt elit magna pariatur deserunt occaecat.</p>
+        <p>Quis ipsum voluptate consequat amet tempor pariatur. Officia laboris amet tempor sint laborum adipisicing. Laboris laboris culpa aliqua anim voluptate velit dolor dolore quis in. Sint incididunt cillum ut Lorem ad esse ad occaecat commodo exercitation aliquip. Non enim labore elit mollit Lorem laborum labore ut cupidatat nulla eiusmod magna aliquip eiusmod. Ullamco aliquip labore in eu non velit sunt elit magna pariatur deserunt occaecat.</p>
         <div id='dark-mode-wrapper'>
-            <label htmlFor='dark-mode-toggle'>Dark Mode:</label>
-            <Toggle id='dark-mode-toggle' className='dark-mode-toggle' name='darkMode' value="yes" checked={darkMode} onChange={() => setDarkMode(prev => !prev)} />
+            <div>Dark Mode:</div>
+            <Toggle className='dark-mode-toggle' name='darkMode' value="yes" checked={darkMode} onChange={() => setDarkMode(prev => !prev)} />
         </div>
     </footer>
   )
